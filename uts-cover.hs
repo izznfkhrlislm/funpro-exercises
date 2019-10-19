a9c653e3948f6e883d42b8966b3d11aefe25dd1c
Izzan Fakhril Islam <izzan.fakhril@ui.ac.id>
2019-10-19 14:56:32 +0700

    adding robot

--- uts-cover.hs
+++ uts-cover.hs
@@ -24,3 +24,21 @@ evaluate (e1 :* e2) = evaluate e1 * evaluate e2
 evaluate (e1 :/ e2) = evaluate e1 / evaluate e2
 evaluate (Let v e0 e1) = evaluate (subst v e0 e1)
 evaluate (V _) = 0.0
+
+-- Imperative Robot
+data Direction = North | East | South | West
+    deriving (Eq, Show, Enum)
+
+right :: Direction -> Direction
+right d = toEnum (succ (fromEnum d) `mod` 4)
+
+data RobotState = RobotState { position :: Position , facing :: Direction
+                                , pen :: Bool , color :: Color
+                                , treasure :: [Position] , pocket :: Int
+                             } deriving Show
+newtype Robot a = Robot (RobotState -> Grid -> Window -> IO (RobotState, a))
+
+turnRight :: Robot ()
+turnRight = updateState (\s -> s {facing = right (facing s)})
+moven :: Int -> Robot ()
+moven n = mapM_ (const move) [ 1 .. n ]
